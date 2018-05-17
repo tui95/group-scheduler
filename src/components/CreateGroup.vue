1e8dbf68b81f09b3f88b6ebc24b7f4e840969a82
DESKTOP-3JDRVSO\User <chaninac127@gmail.com>
2018-05-17 12:20:59 +0700

    add in group_description

--- src/components/CreateGroup.vue
+++ src/components/CreateGroup.vue
@@ -28,6 +28,18 @@
                                         required>
                                     </v-text-field>
                                 </v-flex>
+
+                                <v-flex>
+                                    <v-text-field
+                                        name = "group_description"
+                                        label = "Group Description"
+                                        v-model="group_description"
+                                        prepend-icon = "event_note"
+                                        multi-line
+                                    >
+                                    </v-text-field>
+                                </v-flex>
+
                                 <v-divider></v-divider>
                                 <v-flex class="text-xs-right" mt-2>
                                     <v-btn color="primary" type="submit">Create</v-btn>
@@ -38,34 +50,6 @@
                     </v-card-text>
                 </v-card>
             </v-flex>
-            <!-- <v-flex xs12 class="text-xs-center" mt-5>
-                <h1>Create Group</h1>
-            </v-flex>
-            <v-flex xs12 sm6 offset-sm3 mt-3>
-                <form @submit.prevent="userCreateGroup">
-                    <v-layout column>
-                        <v-flex>
-                            <v-alert type="error" dismissible v-model="alert">
-                                {{ error }}
-                            </v-alert>
-                        </v-flex>
-
-                        <v-flex>
-                            <v-text-field
-                                name="group_name"
-                                label="Group Name"
-                                id="group_name"
-                                type="text"
-                                v-model="group_name"
-                                required>
-                            </v-text-field>
-                        </v-flex>
-                        <v-flex class="text-xs-center" mt-5>
-                            <v-btn color="primary" type="submit">Create</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </form>
-            </v-flex> -->
         </v-layout>
     </v-container>
 </template>
@@ -75,12 +59,16 @@
         data() {
             return {
                 group_name: '',
+                group_description: '',
                 alert: false
             }
         },
         methods: {
             userCreateGroup() {
-                this.$store.dispatch('userCreateGroup', {group_name : this.group_name})
+                this.$store.dispatch('userCreateGroup', {group_name : this.group_name, group_description : this.group_description})
+                this.group_name = ''
+                this.group_description = ''
+                alert("Group succesfully created")
             }
         },
         computed: {
